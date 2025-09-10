0b43517fa33cb5b1280a763b2a4caf5f15b675d9
Whiskyrie <evandroropfilho@gmail.com>
2025-09-10 13:24:02 -0300

    feat: add deliveries, drivers, incidents, reports, routes, tracking, and vehicles modules with basic CRUD functionality.

--- nexus-backend/src/app.module.ts
+++ nexus-backend/src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { VehiclesModule } from './modules/vehicles/vehicles.module';
+import { DriversModule } from './modules/drivers/drivers.module';
+import { RoutesModule } from './modules/routes/routes.module';
+import { DeliveriesModule } from './modules/deliveries/deliveries.module';
+import { TrackingModule } from './modules/tracking/tracking.module';
+import { IncidentsModule } from './modules/incidents/incidents.module';
+import { CustomersModule } from './modules/customers/customers.module';
+import { ReportsModule } from './modules/reports/reports.module';
 
 @Module({
-  imports: [],
+  imports: [
+    VehiclesModule,
+    DriversModule,
+    RoutesModule,
+    DeliveriesModule,
+    TrackingModule,
+    IncidentsModule,
+    CustomersModule,
+    ReportsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
