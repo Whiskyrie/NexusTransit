cd624810863108596c8d0e63310ce0fce3c83ec4
Whiskyrie <evandroropfilho@gmail.com>
2025-09-11 11:27:24 -0300

    feat: adicionar entidade base com campos comuns e métodos de soft delete

---
+++ nexus-backend/src/common/enums/index.ts
@@ -0,0 +1,10 @@
+// User related enums
+export { UserStatus } from './user-status.enum';
+export { UserType } from './user-type.enum';
+
+// Role related enums
+export { RoleType } from './role-type.enum';
+
+// Vehicle related enums
+export { VehicleStatus } from './vehicle-status.enum';
+export { VehicleType } from './vehicle-type.enum';
