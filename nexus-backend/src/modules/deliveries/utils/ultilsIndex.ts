00c72c2194ae3d865c4e04570d2f4fca4e6e6b62
Whiskyrie <evandroropfilho@gmail.com>
2025-10-05 11:23:56 -0300

    feat: adiciona arquivos de exportação para utilitários e validadores

---
+++ nexus-backend/src/modules/deliveries/utils/ultilsIndex.ts
@@ -0,0 +1,8 @@
+/**
+ * Exporta todos os utilitários do módulo deliveries
+ */
+
+export * from './delivery.util';
+export * from './address.util';
+export * from './time-window.util';
+export * from './distance-calculator.util';
---
+++ nexus-backend/src/modules/deliveries/validators/validatorIndex.ts
@@ -0,0 +1,8 @@
+/**
+ * Exporta todos os validators customizados do módulo deliveries
+ */
+
+export * from './tracking-code.validator';
+export * from './address.validator';
+export * from './coordinates.validator';
+export * from './phone-number-br.validator';
