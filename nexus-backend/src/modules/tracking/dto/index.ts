083f3a99a020939050500fee7f3dfca8eeb093dd
Gugalzg <luizgustavoguga15@gmail.com>
2025-10-27 14:58:25 -0300

    feat: implementação index de tracking

---
+++ nexus-backend/src/modules/tracking/dto/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Barrel export para DTOs do módulo de rastreamento
+ */
+
+export * from './create-tracking.dto';
+export * from './update-tracking.dto';
+export * from './tracking-filter.dto';
+export * from './tracking-response.dto';
+export * from './tracking-stats.dto';
