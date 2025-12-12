46cefedf09dbfbd8c15644d4b1b3bf7897df0490
Whiskyrie <evandroropfilho@gmail.com>
2025-12-11 21:11:42 -0300

    refactor(api): update RedisModule import to @nexus/redis

--- apps/api/src/health/health.module.ts
+++ apps/api/src/health/health.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { TerminusModule } from '@nestjs/terminus';
 import { HealthController } from './health.controller';
 import { RedisHealthIndicator } from './indicators/redis.indicator';
-import { RedisModule } from '../modules/redis/redis.module';
+import { RedisModule } from '@nexus/redis';
 
 @Module({
   imports: [TerminusModule, RedisModule],
