93ea20ada44563ad3525334771fec74f49743789
Whiskyrie <evandroropfilho@gmail.com>
2025-12-17 20:37:23 -0300

    feat(drivers): registrar novos serviços e controladores no módulo de Drivers

--- apps/api/src/modules/drivers/drivers.module.ts
+++ apps/api/src/modules/drivers/drivers.module.ts
@@ -3,10 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { DriversService } from './drivers.service';
 import { DriversController } from './drivers.controller';
+import { DriverAvailabilityController } from './controllers/driver-availability.controller';
 import { DriverDocument } from './entities/driver-document.entity';
 import { Driver } from './entities/driver.entity';
 import { DriverLicense } from './entities/driver-license.entity';
+import { DriverAvailability } from './entities/driver-availability.entity';
 import { DriverLicenseService } from './services/driver-license.service';
+import { DriverAvailabilityService } from './services/driver-availability.service';
 
 // Módulos e utils externos
 import { AuditModule, AuditableUtils } from '@nexus/audit';
@@ -19,17 +22,22 @@ import { LicenseValidationInterceptor } from './interceptors/license-validation.
 // Subscribers
 import { DriverLicenseSubscriber } from './subscribers/driver-license.subscriber';
 import { DriverDocumentSubscriber } from './subscribers/driver-document.subscriber';
+import { DriverAvailabilitySubscriber } from './subscribers/driver-availability.subscriber';
 
 // Utils internos
 import { ClsAuditUtils } from './utils/cls-audit.util';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Driver, DriverLicense, DriverDocument]), AuditModule],
-  controllers: [DriversController],
+  imports: [
+    TypeOrmModule.forFeature([Driver, DriverLicense, DriverDocument, DriverAvailability]),
+    AuditModule,
+  ],
+  controllers: [DriversController, DriverAvailabilityController],
   providers: [
     // Services
     DriversService,
     DriverLicenseService,
+    DriverAvailabilityService,
 
     // Utils
     ClsAuditUtils,
@@ -52,7 +60,8 @@ import { ClsAuditUtils } from './utils/cls-audit.util';
     // Subscribers
     DriverLicenseSubscriber,
     DriverDocumentSubscriber,
+    DriverAvailabilitySubscriber,
   ],
-  exports: [DriversService, DriverLicenseService, TypeOrmModule],
+  exports: [DriversService, DriverLicenseService, DriverAvailabilityService, TypeOrmModule],
 })
 export class DriversModule {}
