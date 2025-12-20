b97b758e40f1203075177ff899ee9e4ce4333c10
Whiskyrie <evandroropfilho@gmail.com>
2025-12-20 18:54:32 -0300

    fix(packages): ajustar injeção de Reflector e ClsModule em pacotes compartilhados

--- packages/audit/src/audit.module.ts
+++ packages/audit/src/audit.module.ts
@@ -1,13 +1,15 @@
 import { Module, Global } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { ClsModule } from "nestjs-cls";
+import { Reflector } from "@nestjs/core";
 import { AuditService } from "./audit.service";
 import { AuditSubscriber } from "./subscribers/audit.subscriber";
 import { AuditLogEntity } from "./entities/audit-log.entity";
 
 @Global()
 @Module({
-  imports: [TypeOrmModule.forFeature([AuditLogEntity])],
-  providers: [AuditService, AuditSubscriber],
+  imports: [TypeOrmModule.forFeature([AuditLogEntity]), ClsModule],
+  providers: [AuditService, AuditSubscriber, Reflector],
   exports: [AuditService, TypeOrmModule],
 })
 export class AuditModule {}
--- packages/compliance/src/compliance.module.ts
+++ packages/compliance/src/compliance.module.ts
@@ -1,15 +1,17 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { Reflector } from "@nestjs/core";
 import { ComplianceController } from "./compliance.controller";
 import { ConsentService } from "./consent.service";
 import { DataRequestService } from "./data-request.service";
 import { DataPortabilityService } from "./data-portability.service";
 import { UserConsentEntity, DataRequestEntity } from "./entities/lgpdEntities";
+import { RolesGuard } from "@nexus/auth";
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserConsentEntity, DataRequestEntity])],
   controllers: [ComplianceController],
-  providers: [ConsentService, DataRequestService, DataPortabilityService],
+  providers: [ConsentService, DataRequestService, DataPortabilityService, RolesGuard, Reflector],
   exports: [ConsentService, DataRequestService, DataPortabilityService],
 })
 export class ComplianceModule {}
