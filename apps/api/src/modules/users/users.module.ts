2f6d9c351ac682bcc92b14e42b9842f712cf5c0e
Whiskyrie <evandroropfilho@gmail.com>
2025-12-31 23:06:00 -0300

    chore(users): registrar novos providers e exports no UsersModule

--- apps/api/src/modules/users/users.module.ts
+++ apps/api/src/modules/users/users.module.ts
@@ -4,10 +4,35 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { User } from './entities/user.entity';
 
+// Services auxiliares
+import { UserValidationService } from './services/user-validation.service';
+import { UserSearchService } from './services/user-search.service';
+
+// Subscribers
+import { UserSubscriber } from './subscribers/user.subscriber';
+
+// Validators
+import { IsUniqueEmailConstraint } from './validators/unique-email.validator';
+import { IsStrongPasswordConstraint } from './validators/strong-password.validator';
+
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [UsersService],
-  exports: [UsersService, TypeOrmModule], // Exporta para outros módulos poderem usar
+  providers: [
+    // Service principal
+    UsersService,
+
+    // Services auxiliares
+    UserValidationService,
+    UserSearchService,
+
+    // Subscribers
+    UserSubscriber,
+
+    // Validators
+    IsUniqueEmailConstraint,
+    IsStrongPasswordConstraint,
+  ],
+  exports: [UsersService, UserValidationService, UserSearchService, TypeOrmModule],
 })
 export class UsersModule {}
