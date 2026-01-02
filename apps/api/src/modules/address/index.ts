f394a889abaac1c2cf57913523aa31d82ea4bd7d
Whiskyrie <evandroropfilho@gmail.com>
2026-01-01 21:16:02 -0300

    chore(address): adicionar barrel exports e index principal

---
+++ apps/api/src/modules/address/index.ts
@@ -0,0 +1,48 @@
+/**
+ * Exports centralizados do módulo Address
+ *
+ * Facilita imports externos organizando todas as exportações em um único ponto
+ */
+
+// Module
+export * from './address.module';
+
+// Controller e Service principais
+export * from './address.controller';
+export * from './address.service';
+
+// Entities
+export * from './entities';
+
+// DTOs
+export * from './dto';
+
+// Enums
+export * from './enums';
+
+// Constants
+export * from './constants';
+
+// Validators
+export * from './validators';
+
+// Exceptions
+export * from './exceptions';
+
+// Interfaces
+export * from './interfaces';
+
+// Utils
+export * from './utils';
+
+// Services auxiliares
+export * from './services';
+
+// Decorators
+export * from './decorators';
+
+// Interceptors
+export * from './interceptors';
+
+// Subscribers
+export * from './subscribers';
