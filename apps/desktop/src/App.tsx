6f7a53f0aa1c831b20c94f06fafbaa28715dd2e7
Whiskyrie <evandroropfilho@gmail.com>
2026-01-07 03:08:41 -0300

    chore: remover página de demo do calendário

--- apps/desktop/src/App.tsx
+++ apps/desktop/src/App.tsx
@@ -5,7 +5,6 @@ import { UsersPage } from "./pages/UsersPage";
 import { DriversPage } from "./pages/DriversPage";
 import { VehiclesPage } from "./pages/VehiclesPage";
 import { RoutesPage } from "./pages/RoutesPage";
-import { CalendarDemoPage } from "./pages/CalendarDemoPage";
 import { ProtectedRoute } from "./components/ProtectedRoute";
 import { AppLayout } from "./components/layouts";
 import { useIsAuthenticated } from "./stores/auth.store";
@@ -34,7 +33,6 @@ function App() {
           <Route path="/drivers" element={<DriversPage />} />
           <Route path="/vehicles" element={<VehiclesPage />} />
           <Route path="/routes" element={<RoutesPage />} />
-          <Route path="/calendar-demo" element={<CalendarDemoPage />} />
           {/* Adicione mais rotas protegidas aqui */}
         </Route>
       </Route>
